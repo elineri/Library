abc72828503bc9048c37e48ce730bd2ff35f8d5e
elineri <elin.ericstam@gmail.com>
2022-09-01 14:58:17 +0200

    Customer

--- Library/Models/LoanDetail.cs
+++ Library/Models/LoanDetail.cs
@@ -4,11 +4,5 @@ namespace Library.Models
 {
     public class LoanDetail
     {
-        [Key]
-        public int LoanDetailId { get; set; }
-        public int LoanId { get; set; }
-        public Loan Loan { get; set; }
-        public int BookId { get; set; }
-        public Book Book { get; set; }
     }
 }
