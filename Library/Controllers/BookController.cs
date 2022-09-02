ce15a9aacf05d0b35ae45a6c4e11488ef6f40c9d
elineri <elin.ericstam@gmail.com>
2022-09-02 09:49:05 +0200

    Created BookController and Views

---
+++ Library/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_bookRepository.GetAllBooks);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var book = _bookRepository.GetBookById(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+    }
+}
--- Library/Startup.cs
+++ Library/Startup.cs
@@ -29,6 +29,7 @@ namespace Library
 
             services.AddControllersWithViews();
             services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
