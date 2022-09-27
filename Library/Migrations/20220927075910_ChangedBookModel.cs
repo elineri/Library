3a4490b04a5d495315fdf0a9e182aaa944bdb2a2
elineri <elin.ericstam@gmail.com>
2022-09-27 10:03:19 +0200

    Customer list ordered by last name

---
+++ Library/Migrations/20220927075910_ChangedBookModel.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Library.Migrations
+{
+    public partial class ChangedBookModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LoanCartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Loans_BookId",
+                table: "Loans");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Loans_BookId",
+                table: "Loans",
+                column: "BookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Loans_BookId",
+                table: "Loans");
+
+            migrationBuilder.CreateTable(
+                name: "LoanCartItems",
+                columns: table => new
+                {
+                    LoanCartItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: true),
+                    LoanCartId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoanCartItems", x => x.LoanCartItemId);
+                    table.ForeignKey(
+                        name: "FK_LoanCartItems_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Loans_BookId",
+                table: "Loans",
+                column: "BookId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoanCartItems_BookId",
+                table: "LoanCartItems",
+                column: "BookId");
+        }
+    }
+}
